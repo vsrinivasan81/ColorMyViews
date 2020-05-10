2f688cbfd65776d16cc484895ded7cb7235bf906
Vijay Srinivasan <cbevijay@gmail.com>
2020-05-10 13:14:21 +0530

    Reverted changes for setting background images

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -17,20 +17,19 @@ class MainActivity : AppCompatActivity() {
     private fun makeColored(view: View) {
         when (view.id) {
             // Boxes using Color class colors for the background
-            /*
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.RED)
 
-             */
 
-            R.id.box_one_text -> view.setBackgroundResource(R.drawable.image_1)
+            /*R.id.box_one_text -> view.setBackgroundResource(R.drawable.image_1)
             R.id.box_two_text -> view.setBackgroundResource(R.drawable.image_2)
             R.id.box_three_text -> view.setBackgroundResource(R.drawable.image_3)
             R.id.box_four_text -> view.setBackgroundResource(R.drawable.image_4)
             R.id.box_five_text -> view.setBackgroundResource(R.drawable.image_5)
+             */
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
@@ -45,8 +44,10 @@ class MainActivity : AppCompatActivity() {
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
         val clickableViews: List<View> =
-            listOf(boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout)
+            listOf(
+                boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText, rootConstraintLayout
+            )
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
