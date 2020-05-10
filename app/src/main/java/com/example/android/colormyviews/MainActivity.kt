35925cf938a66c79ea88ec3d94c518cda327e1a9
Vijay Srinivasan <cbevijay@gmail.com>
2020-05-10 13:12:37 +0530

    Set background image instead of background color

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -17,11 +17,21 @@ class MainActivity : AppCompatActivity() {
     private fun makeColored(view: View) {
         when (view.id) {
             // Boxes using Color class colors for the background
+            /*
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.RED)
+
+             */
+
+            R.id.box_one_text -> view.setBackgroundResource(R.drawable.image_1)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.image_2)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.image_3)
+            R.id.box_four_text -> view.setBackgroundResource(R.drawable.image_4)
+            R.id.box_five_text -> view.setBackgroundResource(R.drawable.image_5)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
